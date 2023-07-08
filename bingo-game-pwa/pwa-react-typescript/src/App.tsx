b04a4535fa10b8b1243164f35fc7cdaa098df5f3
c-ctkd <cmcm1995@hotmail.com>
2023-07-08 17:38:12 +0100

    wip: cenas

---
+++ bingo-game-pwa/pwa-react-typescript/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BingoBoard } from './components';
+
+function App() {
+  return <BingoBoard />;
+}
+
+export default App;
