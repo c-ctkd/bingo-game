5567399e3eb8474e2e3476bcc33a8e6ce2b31ca0
c-ctkd <cmcm1995@hotmail.com>
2023-07-11 19:43:40 +0100

    wip

--- bingo-game-pwa/pwa-react-typescript/src/components/bingo-board.tsx
+++ bingo-game-pwa/pwa-react-typescript/src/components/bingo-board.tsx
@@ -1,28 +1,32 @@
-import { FC, useEffect, useMemo, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import Modal from 'react-modal';
 import './bingo-board.scss';
 export const BingoBoard: FC = () => {
   const [usedNumbers, setUsedNumbers] = useState<Set<number>>(new Set());
   const [gameOver, setGameOver] = useState<boolean>(false);
+
+  const [current, setCurrent] = useState<number | undefined>(undefined);
   const [openModal, setOpenModal] = useState<boolean>(false);
 
   const getNumber = () => {
     //debugger;
     let picked = false;
     let updatedUsedN = new Set(usedNumbers.values());
+    let curr = 0;
     do {
-      let number = Math.floor(Math.random() * 90) + 1;
+      let number = Math.floor(Math.random() * 75) + 1;
       if (!usedNumbers.has(number)) {
+        curr = number
         updatedUsedN.add(number);
         picked = true;
       }
     } while (!picked);
-
+    setCurrent(curr)
     setUsedNumbers(updatedUsedN);
   };
 
   useEffect(() => {
-    if (usedNumbers.size === 90) {
+    if (usedNumbers.size === 75) {
       setGameOver(true);
     }
   }, [usedNumbers]);
@@ -30,7 +34,7 @@ export const BingoBoard: FC = () => {
   const generateBoard = () => {
     console.log('generating board');
     const board = [];
-    for (let i = 0; i < 90; i++) {
+    for (let i = 0; i < 75; i++) {
       const className = usedNumbers.has(i + 1)
         ? 'number-cell used'
         : 'number-cell';
@@ -75,6 +79,7 @@ export const BingoBoard: FC = () => {
   const handleReset = () => {
     setGameOver(false);
     setUsedNumbers(new Set<number>());
+    setCurrent(undefined)
   };
 
   useEffect(() => {
@@ -82,20 +87,29 @@ export const BingoBoard: FC = () => {
       setOpenModal(true);
     }
   }, [gameOver]);
-  console.log(usedNumbers);
+  console.log(current);
   return (
     <div className="main-content">
-      <h2>Bingo Game</h2>
-      {getSequence()}
-      <div className="bingo-number-container">{generateBoard()}</div>
-      <div className="action-buttons">
-        <button className="action-button" onClick={getNumber}>
-          pick number!
-        </button>
-        <button className="action-button" onClick={handleReset}>
-          Reset!
-        </button>
+      {/*getSequence()*/}
+      <div className='score-container'>
+        <div className="bingo-number-container">
+          {generateBoard()}
+        </div>
+        <div className='display-current'>
+          {current || '-'}
+        </div>
+        <div className="action-buttons">
+          <button className="action-button" onClick={getNumber}>
+            pick number!
+          </button>
+          <button className="action-button" onClick={handleReset}>
+            Reset!
+          </button>
+        </div>
       </div>
+      <div className='logo-container'></div>
+      
+      
       <Modal
         isOpen={openModal}
         onRequestClose={() => setOpenModal(false)}
